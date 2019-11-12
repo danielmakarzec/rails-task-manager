db46a39b8cf0fb902310fbf7f73cc5071949834f
daniel makarzec <danielmakarzec@gmail.com>
2019-11-12 15:03:39 +0000

    challenge completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # .all models
+  get 'tasks', to: 'tasks#index'
+  # create new model
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  # update new model
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks/:id', to: 'tasks#update'
+  # display single model
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  # destroy existing model
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
